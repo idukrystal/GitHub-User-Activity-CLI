5386db7dded38df918822fd2bf34290be14f9319
Christopher Idunoba <idukrystal@gmail.com>
2025-04-02 15:31:21 +0100

    implemented call to github Api to get user activities from provided username arg

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	//"net/http"
+	"net/http"
 	"os"
 )
 
@@ -12,10 +12,22 @@ func main() {
 	args := os.Args
 	defer func() {
 		if r := recover(); r != nil {
+			// args[0] is the name of this program
 			fmt.Println(args[0], ": ", r)
 		}
 	}()
+
+	// if only one arg is provided: no username was entered
 	if len(args) < 2 {
 		panic(NoUsernameError)
 	}
+
+	// args[1] is the provided username
+	userActivityUrl := fmt.Sprintf("https://api.github.com/users/%s/events", args[1])
+	
+	res , err := http.Get(userActivityUrl)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(res)
 }
